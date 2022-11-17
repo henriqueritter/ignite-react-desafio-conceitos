00cc6ddf7cae4c8e68f1e790abe114582a2ff063
henriqueritter <henrique-ritter@hotmail.com>
2022-11-16 21:21:50 -0300

    styles: add styles to textarea form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import styles from './App.module.css'
 import rocketLogo from './assets/rocket.svg';
+import clipboardIco from './assets/clipboard.svg';
+import { PlusCircle } from 'phosphor-react';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src={rocketLogo} alt="Logo Foguete" />
+        <img src={rocketLogo} className={styles.headerLogo} alt="Logo Foguete" />
         <strong className={styles.headerTitle}>to</strong><strong>do</strong>
       </header>
+      <form className={styles.todoForm} onScroll={() => alert('OK')}>
+        <textarea />
+        <button>
+          Criar <PlusCircle size={32} />
+        </button>
+      </form>
+      <div className={styles.contentSection}>
+        <div className='todosContent'>
+          <span>Tarefas criadas 0</span>
+          <span>Concluídas 0</span>
+          --------
+          <div>
+            <img src={clipboardIco} alt="Clipboard" />
+            <span>Você ainda não tem tarefas cadastradas</span>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
