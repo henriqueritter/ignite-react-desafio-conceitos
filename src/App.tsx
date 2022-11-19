13e71b83046646d876095230e0ad3ed516d6c7e0
henriqueritter <henrique-ritter@hotmail.com>
2022-11-19 17:35:46 -0300

    styles: styled todos section

--- src/App.tsx
+++ src/App.tsx
@@ -29,9 +29,10 @@ function App() {
         </div>
 
         <div className={styles.contentTodos}>
+
           <img src={clipboardIco} alt="Clipboard" />
-          <span >Você ainda não tem tarefas cadastradas</span>
-          <span>Crie tarefas e organize seus itens a fazer</span>
+          <span>Você ainda não tem tarefas cadastradas</span>
+          <p>Crie tarefas e organize seus itens a fazer</p>
         </div>
       </div>
     </div>
