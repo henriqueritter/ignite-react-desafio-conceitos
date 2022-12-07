e8d2a923db97803ad1898bd6744d3f04081b4e48
henriqueritter <henrique-ritter@hotmail.com>
2022-12-06 21:46:28 -0300

    feat: Todo base HTML structure

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,24 +1,20 @@
-import { PlusCircle, Trash, Circle, CheckCircle, Student } from 'phosphor-react';
+import { Trash, Circle, CheckCircle } from 'phosphor-react';
 import styles from './styles.module.css'
 
-function Todo() {
+function Todo(props: any) {
   function handleCheckTodo() {
     console.log('Clicou');
   }
 
-  const todos: any = [
-    {
-      id: 1234,
-      content: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer",
-      isChecked: true
-    }
-  ];
+  const todo: any = props.todos;
 
   return (
     <div className={styles.todo}>
-      <CheckCircle onClick={handleCheckTodo} weight="fill" className={styles.checkBox} size={32} />
-      <div className={todos[0].isChecked ? styles.checkedTodo : styles.unCheckedTodo}>
-        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+      {todo.isChecked ?
+        <CheckCircle onClick={handleCheckTodo} weight="fill" className={styles.checkBox} size={32} /> :
+        <Circle onClick={handleCheckTodo} className={styles.checkBox} size={32} />}
+      <div className={todo.isChecked ? styles.checkedTodo : styles.unCheckedTodo}>
+        {todo.content}
       </div>
       <Trash size={16} />
     </div>
